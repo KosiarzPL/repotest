d5cfb6f13925082a7edf88d7cee65e5bd4a31394
kl2ag2 <kl2ag2@komp02.lo1cg.org>
2017-03-08 07:56:36 +0100

    szyfry

---
+++ cpp/vignere.cpp
@@ -0,0 +1,31 @@
+/*
+ * vignere.cpp
+ *
+ *
+ *
+ */
+
+
+using namespace std;
+#include <iostream>
+#include <string>
+
+
+int main(int argc, char **argv)
+{
+    //char tekst[120];
+    string tekst, szyfrogram,odszyfrowany;
+    cout <<"Podaj tekst do zaszyfrowania:";
+    cin >>tekst;
+
+    cout << tekst << endl << "Rozmiar" << tekst. size() << endl;
+    szyfrogram.resize(tekst.size());
+    for (unsigned int i = 0; i<tekst.size(); i++) {
+        cout << tekst[i] << " ";
+     szyfrogram[i] = zakodowany_znak;
+
+    }
+
+	return 0;
+}
+
