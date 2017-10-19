8021d3a2fb584cc5d594aefe91a791caacabede7
KosiarzPL <piobak26@wp.pl>
2017-10-19 08:41:01 +0200

    asdas

--- bazy3/sql/zapytania.py
+++ bazy3/sql/zapytania.py
@@ -6,18 +6,75 @@
 
 import sqlite3
 
+def wyniki(dane):
+    for rekord in dane:
+        print(tuple(rekord))
 
+def kw_h(cur):
+    parametr = input("Podaj numer działu")
+    cur.execute("""
+        SELECT nazwisko,imie,dzial.nazwa,stanowisko,siedziba
+        FROM dzial, pracownicy
+        WHERE dzial.id = pracownicy.id_dzial
+        AND dzial.id = ?
+    """,(parametr,))
+    
+    wyniki(cur.fetchall())
+
+def kw_g(cur):
+    cur.execute("""
+        SELECT imie, nazwisko,
+        CAST ((JulianDay() - JulianDay(data_zatr)) / 365 AS INTEGER) AS okres
+        FROM pracownicy
+        ORDER BY okres DESC
+        LIMIT 10
+    """)
+    
+    wyniki(cur.fetchall())
+
+def kw_f(cur):
+    #~cur.execute("""
+        #~SELECT COUNT(placa)
+        #~FROM pracownicy
+        #~WHERE imie LIKE'%a'
+        
+        
+    #~""")
+    
+    wyniki(cur.fetchall())
+
+    cur.execute("""
+        SELECT AVG(placa)
+        FROM pracownicy
+        GROUP BY imie LIKE '%a'
+        
+        """)
+    wyniki(cur.fetchall())
+        
+    
 
 def kw_c(cur):
     cur.execute("""
-        SELECT SUM(placa) FROM pracownicy
+        SELECT siedziba, SUM(placa) AS pensje
+        FROM dzial, pracownicy
+        WHERE dzial.id = pracownicy.id_dzial
+        GROUP BY siedziba
+        ORDER BY pensje ASC
     """)
     
-    wyniki = cur.fetchall()
-    for rekord in wyniki:
-        print(tuple(rekord))
+    wyniki(cur.fetchall())
+  
 
-
+def kw_e(cur):
+    cur.execute("""
+        SELECT nazwisko, stanowisko, placa * premia.premia
+        FROM pracownicy, premia
+        WHERE pracownicy.stanowisko = premia.id
+        
+    """)
+    
+    wyniki(cur.fetchall())
+   
 
 def kw_d(cur):
     parametr = input('Podaj nazwę działu:')
@@ -30,15 +87,13 @@ def kw_d(cur):
         AND imie NOT LIKE '%a'
     """,(parametr,))
     
-    wyniki = cur.fetchall()
-    for rekord in wyniki:
-        print(tuple(rekord))
-
+    wyniki(cur.fetchall())
+    
 def main(args):
     con = sqlite3.connect('pracownicy.sqlite3')
     cur = con.cursor()   # utworzenie kursora
     
-    kw_d(cur)
+    kw_h(cur)
     
     return 0
 
